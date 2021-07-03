adb1f03f23e0ca3d25501bf1fc183bb57ec07c6c
hansungbin <happynewmind@gmail.com>
2021-07-03 15:22:15 +0900

    0703 시작

--- app/src/main/java/com/example/superstreamer/LoginActivity.kt
+++ app/src/main/java/com/example/superstreamer/LoginActivity.kt
@@ -3,33 +3,59 @@ package com.example.superstreamer
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.example.superstreamer.R.layout.activity_login
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
-    var auth : FirebaseAuth? = null
+    var TAG : String? = "로그 LoginActivity - "
+    var auth: FirebaseAuth? = null
+    var googleSignInClient: GoogleSignInClient? = null
+    var GOOGLE_LOGIN_CODE = 9001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activity_login)
         auth = FirebaseAuth.getInstance()
+        var GOOGLE_LOGIN_CODE = 9001
+
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("497846755588-ekqp507hvubfqk4q8aonlbpqffh1jfem.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
 
         btn_login_email.setOnClickListener {
             signinAndSignup()
         }
+
+        btn_login_google.setOnClickListener {
+            googleLogin()
+        }
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
     }
 
-    fun signinAndSignup(){
+    fun googleLogin() {
+        var signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
+    }
+
+    fun signinAndSignup() {
         auth?.createUserWithEmailAndPassword(et_email.text.toString(), et_password.text.toString())
-            ?.addOnCompleteListener {
-                    task ->
-                if (task.isSuccessful){
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
                     //Creating a user account
                     moveMainPage(task.result?.user)
-                }else {
+                } else {
                     //Login if you have account
                     signinEmail()
                 }
@@ -50,9 +76,44 @@ class LoginActivity : AppCompatActivity() {
             }
     }
 
-    fun moveMainPage(user: FirebaseUser?){
-        if(user!=null) {
+    fun moveMainPage(user: FirebaseUser?) {
+        if (user != null) {
             startActivity(Intent(this, MainActivity::class.java))
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GOOGLE_LOGIN_CODE) {
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if (result!!.isSuccess) {
+                var account = result.signInAccount
+                //Second step
+                firebaseAuthWithGoogle(account)
+
+            }
+        }
+    }
+
+    fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
+        var credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    //Login
+                    moveMainPage(task.result?.user)
+                } else {
+                    //show the error message
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+
+                }
+            }
+    }
+
+    //자동로그인기능
+    override fun onStart() {
+        Log.d(TAG, "onStart()")
+        super.onStart()
+        moveMainPage(auth?.currentUser)
     }
 }
--- app/src/main/java/com/example/superstreamer/MainActivity.kt
+++ app/src/main/java/com/example/superstreamer/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.superstreamer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    var fragmentView: View? = null
+    var TAG : String? = "로그 MainActivity - "
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var stationListActivity = StationListActivity()
+        supportFragmentManager.beginTransaction().replace(R.id.main_content, stationListActivity).commit()
+
+//        var intent = Intent(this, StationListActivity::class.java)
+//        startActivity(intent)
+
     }
+
+
+//    var detailViewFragment = DetailViewFragment()
+//    supportFragmentManager.beginTransaction().replace(R.id.main_content, detailViewFragment).commit()
+//    return true
+
+    //        var stationListActivity = StationListActivity()
+//        supportFragmentManager.beginTransaction().replace(R.id.main_content, stationListActivity).commit()
+//        return true
+//
+//        viewholder.detailViewItem_comment_imageview.setOnClickListener { v ->
+//            var intent = Intent(v.context, CommentActivity::class.java)
+//            intent.putExtra("contentUid", contentUidList[position])
+//            intent.putExtra("destinationUid", contentDTOs[position].uid)
+//            startActivity(intent)
+//        }
 }
