9ba7681ba719e82ebfb51f967fc1e12fc85cf71a
hansungbin <happynewmind@gmail.com>
2021-07-05 21:45:06 +0900

    0704 21:44 코딩종료 cafe24에서 데이터를 갖고 오기 위한 JSON 통신을 위한 라이브러리 추가 완료

--- app/src/main/java/com/example/superstreamer/navigation/StationFragment.kt
+++ app/src/main/java/com/example/superstreamer/navigation/StationFragment.kt
@@ -5,10 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.superstreamer.R
+import com.google.firebase.firestore.FirebaseFirestore
 
 class StationFragment : Fragment() {
     var TAG : String? = "로그 StationFragment - "
+    var firestore : FirebaseFirestore? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,7 +19,25 @@ class StationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_station,container, false)
-
+        firestore = FirebaseFirestore.getInstance()
         return view
+    }
+
+    inner class StationViewRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+
+        override fun getItemCount(): Int {
+            TODO("Not yet implemented")
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            TODO("Not yet implemented")
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            TODO("Not yet implemented")
+        }
+
+
+
     }
 }
